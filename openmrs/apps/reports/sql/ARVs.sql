aaa66b5c5112e41ebf4c7be7b14f2ecbab84f9b1
ksetetemela <khobatha.setetemela@gmail.com>
2022-11-10 21:58:34 +0200

    handle null expiry dates in ARVs.sql

--- openmrs/apps/reports/sql/ARVs.sql
+++ openmrs/apps/reports/sql/ARVs.sql
@@ -1,6 +1,6 @@
 SELECT Product_Name, Unit_of_Issue,
 	 CASE WHEN Product_Name IS NULL THEN 0 ELSE (SUM(CASE WHEN stock_on_hand>0 THEN stock_on_hand ELSE 0 END)) END AS Qty_Stock_on_Hand,
-	 Expiry_Date,
+	 CASE WHEN Expiry_Date IS NULL THEN 0 ELSE Expiry_Date END AS Stock_Expiry_Date,
 	 CASE WHEN Product_Name IS NULL THEN 0 ELSE (SUM(CASE WHEN Quantity_expired_and_damaged>0 THEN Quantity_expired_and_damaged ELSE 0 END)) END AS Qty_Expired_and_damaged,
      CASE WHEN Product_Name IS NULL THEN 0 ELSE (SUM(CASE WHEN Quantity_Received>0 THEN Quantity_Received ELSE 0 END)) END AS Qty_Received,
      CASE WHEN Product_Name IS NULL THEN 0 ELSE (SUM(CASE WHEN stock_at_last_reporting_period>0 THEN stock_at_last_reporting_period ELSE 0 END)) END AS Qty_stock_at_last_reporting_period
