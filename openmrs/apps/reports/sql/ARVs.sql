a8e33fb5d79e66141df506c5d5e367987849f8e0
ksetetemela <khobatha.setetemela@gmail.com>
2023-03-05 17:24:39 +0200

    refactoring

--- openmrs/apps/reports/sql/ARVs.sql
+++ openmrs/apps/reports/sql/ARVs.sql
@@ -1,45 +1,45 @@
 SELECT Product_description, sum(Stock_on_hand) as "Stock_on_Hand", min(Expiry_Date) as "Expiry Date", sum(Quantity_expired_and_damaged) as "Quantity Expired/Damaged", sum(Quantity_Received) as "Quantity Received"
 FROM
 (
-(Select Product_description, Stock_on_hand, Expiry_Date ,damaged.Quantity_expired_and_damaged ,Quantity_Received
-From
-    (select Id, name Product_description,sum(stock_on_hand) Stock_on_hand
+(SELECT Product_description, Stock_on_hand, Expiry_Date ,damaged.Quantity_expired_and_damaged ,Quantity_Received
+FROM
+    (SELECT Id, name Product_description,sum(stock_on_hand) Stock_on_hand
         FROM
         (-- STOCK QUANTITY
-            select distinct template.id as Id,SQuatity.qty stock_on_hand ,template.name
+            SELECT DISTINCT template.id AS Id,SQuatity.qty stock_on_hand ,template.name
             FROM product_template template
-            INNER JOIN stock_quant SQuatity on SQuatity.product_id = template.id
+            INNER JOIN stock_quant SQuatity ON SQuatity.product_id = template.id
         AND CAST(SQuatity.write_date AS DATE) >= CAST('2023-02-01' AS DATE)
         AND CAST(SQuatity.write_date AS DATE) <= CAST('2023-02-28' AS DATE)
             ) product_name_quant
-        Group by product_name_quant.name,product_name_quant.id) as stock_pro
+        GROUP BY product_name_quant.name,product_name_quant.id) AS stock_pro
         LEFT OUTER JOIN
         (-- OLDEST EXPIRY DATE
-		select product_id,min(expiry_date) Expiry_Date
+		SELECT product_id,min(expiry_date) Expiry_Date
         FROM stock_pack_operation_lot sp
         INNER JOIN stock_pack_operation spo on spo.id = operation_id
         AND spo.write_date >= CAST('2023-02-01' AS DATE)
         AND spo.write_date <= CAST('2023-02-28' AS DATE)
-        group by product_id
+        GROUP BY product_id
         )expiry ON stock_pro.Id = expiry.product_id
 LEFT OUTER JOIN
 ( -- EXPIRED/DAMAGED
-select product_id,Quantity_Expired Quantity_expired_and_damaged
+SELECT product_id,Quantity_Expired Quantity_expired_and_damaged
 FROM
 (
     (
-    select distinct sp.product_id,sum(qty) as Quantity_Expired
-    from stock_pack_operation sp
+    SELECT DISTINCT sp.product_id,sum(qty) AS Quantity_Expired
+    FROM stock_pack_operation sp
     INNER JOIN stock_pack_operation_lot spo on sp.id = spo.operation_id AND expiry_date < CAST('2023-02-28' AS DATE)
     AND sp.write_date >= CAST('2023-02-01' AS DATE)
     AND sp.write_date <= CAST('2023-02-28' AS DATE)
-    group by sp.product_id
+    GROUP BY sp.product_id
     )
 UNION ALL
     (
-    select distinct ss.product_id,sum(scrap_qty)
-    from stock_pack_operation sp
-    INNER JOIN stock_scrap ss on sp.product_id = ss.product_id
+    SELECT DISTINCT ss.product_id,sum(scrap_qty)
+    FROM stock_pack_operation sp
+    INNER JOIN stock_scrap ss ON sp.product_id = ss.product_id
     AND sp.write_date >= CAST('2023-02-01' AS DATE)
     AND sp.write_date <= CAST('2023-02-28' AS DATE)
     GROUP BY ss.product_id
