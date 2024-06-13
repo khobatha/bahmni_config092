ec4a3c973f4d82f1b4787a8604083a769969ea9a
khobatha <khobatha.setetemela@gmail.com>
2024-06-13 04:41:37 +0000

    shorted TaT report v1

--- openmrs/apps/reports/sql/Test_Sent_Turn_Around_Time_VL.sql
+++ openmrs/apps/reports/sql/Test_Sent_Turn_Around_Time_VL.sql
@@ -1,50 +1,27 @@
-SELECT "Average Turn Around Time" as "Average Turn Around Time"
+SELECT "Average Turn Around Time" as "Average Turn Around Time",
+       round((SUM(turn_around_time)/COUNT(*)), 1) as "Average Turn Around Time"
 FROM (
-    SELECT 
-        round((SUM(datediff(result_date, date_collected))/COUNT(*)), 1) as "Average Turn Around Time"
+    SELECT datediff(result_date, date_collected) as turn_around_time
     FROM (
-        SELECT 
-            datediff(result_date, date_collected) as turn_around_time
-        FROM (
-            SELECT DISTINCT 
-                patient.patient_id AS Id,
-                patient_identifier.identifier AS patientIdentifier,
-                concat(person_name.given_name, " ", person_name.family_name) AS patientName,
-                floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
-                person.gender AS Gender,
-                cast(o.date_created as date) as date_collected,
-                observed_age_group.name AS age_group,
-                "Done" AS Test,
-                observed_age_group.sort_order AS sort_order,
-                order_id
-            FROM orders o
-            INNER JOIN patient ON o.patient_id = patient.patient_id 
-            INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-            INNER JOIN person_name ON person.person_id = person_name.person_id
-            INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
-            INNER JOIN reporting_age_group AS observed_age_group ON
-                CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
-                AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-            AND o.concept_id = 5484
-            WHERE observed_age_group.report_group_name = 'Modified_Ages'
-            AND CAST(o.date_created AS DATE) >= CAST('#startDate#' AS DATE)
-            AND CAST(o.date_created AS DATE) <= CAST('#endDate#' AS DATE)
-        ) as test
+        SELECT DISTINCT 
+            o.order_id,
+            o.date_created as date_collected,
+            vl.result_date
+        FROM orders o
+        INNER JOIN patient ON o.patient_id = patient.patient_id 
+        INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+        INNER JOIN person_name ON person.person_id = person_name.person_id
+        INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+        INNER JOIN reporting_age_group AS observed_age_group ON
+            CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+            AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+        AND o.concept_id = 5484
         LEFT JOIN (
             SELECT 
-                person_id, 
-                Lab_Order_Number, 
-                lab_order.order_id as orders_id, 
-                VL_result.order_id, 
-                VL_result.Results, 
-                result_date 
+                lab_order.order_id,
+                VL_result.result_date
             FROM (
-                SELECT 
-                    person_id, 
-                    cast(obs_datetime as date) as max_observation, 
-                    SUBSTRING(CONCAT(obs_datetime, obs_id), 20) AS observation_id, 
-                    order_id, 
-                    value_text as Lab_Order_Number
+                SELECT order_id, value_text as Lab_Order_Number
                 FROM obs 
                 WHERE concept_id = 5498
                 AND cast(obs_datetime as date) >= cast('#startDate#' as date)
@@ -53,34 +30,18 @@ FROM (
             ) lab_order
             LEFT JOIN (
                 SELECT 
-                    pId, 
-                    result.order_id, 
-                    Results, 
+                    result.order_id,
                     cast(obs_datetime as date) as result_date
-                FROM (
-                    SELECT 
-                        oss.person_id as pId, 
-                        concat(oss.value_numeric, " ", "copies/ml") as Results, 
-                        order_id, 
-                        oss.obs_datetime
-                    FROM obs oss
-                    WHERE oss.concept_id = 5485
-                    AND oss.voided = 0
-                    AND cast(oss.obs_datetime as date) >= cast('#startDate#' as date)
-                    UNION
-                    SELECT 
-                        oss.person_id as pId, 
-                        "LDL" as Results, 
-                        order_id, 
-                        oss.obs_datetime
-                    FROM obs oss
-                    WHERE oss.concept_id = 5489
-                    AND oss.voided = 0
-                    AND cast(oss.obs_datetime as date) >= cast('#startDate#' as date)
-                ) result
+                FROM obs result
+                WHERE (result.concept_id = 5485 OR result.concept_id = 5489)
+                AND result.voided = 0
+                AND cast(result.obs_datetime as date) >= cast('#startDate#' as date)
             ) VL_result
             ON lab_order.order_id = VL_result.order_id
-        ) lab_orders
-        ON test.order_id = lab_orders.orders_id
-    ) tests
-) lab_orders_with_TAT;
+        ) vl
+        ON o.order_id = vl.order_id
+        WHERE observed_age_group.report_group_name = 'Modified_Ages'
+        AND CAST(o.date_created AS DATE) >= CAST('#startDate#' AS DATE)
+        AND CAST(o.date_created AS DATE) <= CAST('#endDate#' AS DATE)
+    ) as test
+) as final;
